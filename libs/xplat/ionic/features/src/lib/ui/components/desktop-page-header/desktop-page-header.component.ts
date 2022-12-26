c9228eef9bc35757cfe8381da057c11d7a7eee69
Kenny Tat <kennytat.app@gmail.com>
2022-12-26 10:19:30 +0700

    fix(topbar-searchbar): fixed topbar and searchbar css

--- libs/xplat/ionic/features/src/lib/ui/components/desktop-page-header/desktop-page-header.component.ts
+++ libs/xplat/ionic/features/src/lib/ui/components/desktop-page-header/desktop-page-header.component.ts
@@ -1,169 +1,169 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import {
-  BaseComponent,
-  DataFetchService,
-  Item,
-  QueueService,
+	BaseComponent,
+	DataFetchService,
+	Item,
+	QueueService,
 } from '@fy/xplat/core';
 import { Location } from '@angular/common';
 import { PlayerService } from 'libs/xplat/core/src/lib/services/player.service';
 import * as path from 'path';
 
 @Component({
-  selector: 'fy-desktop-page-header',
-  templateUrl: 'desktop-page-header.component.html',
-  styleUrls: ['./desktop-page-header.component.scss'],
+	selector: 'fy-desktop-page-header',
+	templateUrl: 'desktop-page-header.component.html',
+	styleUrls: ['./desktop-page-header.component.scss'],
 })
 export class DesktopPageHeaderComponent extends BaseComponent {
-  @Input() home: string = 'video';
-  searchOnFocus = false;
-  searchResult: any = {};
-  searchQuery: string = '';
-  searchValue: string = '';
-  isSearch: boolean;
-  isVideo: boolean = true;
-  constructor(
-    private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private location: Location,
-    private playerService: PlayerService,
-    public dataFetchService: DataFetchService,
-    private queueService: QueueService
-  ) {
-    super();
-  }
+	@Input() home: string = 'video';
+	searchOnFocus = false;
+	searchResult: any = {};
+	searchQuery: string = '';
+	searchValue: string = '';
+	isSearch: boolean;
+	isVideo: boolean = true;
+	constructor(
+		private router: Router,
+		private activatedRoute: ActivatedRoute,
+		private location: Location,
+		private playerService: PlayerService,
+		public dataFetchService: DataFetchService,
+		private queueService: QueueService
+	) {
+		super();
+	}
 
-  searchModeChange() {
-    this.isVideo = !this.isVideo;
-    this.searchValue = '';
-  }
+	searchModeChange() {
+		this.isVideo = !this.isVideo;
+		this.searchValue = '';
+	}
 
-  async searchChange(e?) {
-    const index = this.dataFetchService.searchClient.index(
-      this.dataFetchService.searchDatabase
-    );
-    this.searchQuery = e.detail.value;
-    try {
-      this.searchResult =
-        this.isVideo === true
-          ? await index.search(e.detail.value, {
-            filter: 'isVideo = true',
-            limit: 20,
-          })
-          : await index.search(e.detail.value, {
-            filter: 'isVideo = false',
-            limit: 20,
-          });
+	async searchChange(e?) {
+		const index = this.dataFetchService.searchClient.index(
+			this.dataFetchService.searchDatabase
+		);
+		this.searchQuery = e.detail.value;
+		try {
+			this.searchResult =
+				this.isVideo === true
+					? await index.search(e.detail.value, {
+						filter: 'isVideo = true',
+						limit: 20,
+					})
+					: await index.search(e.detail.value, {
+						filter: 'isVideo = false',
+						limit: 20,
+					});
 
-      console.log(this.searchResult);
+			console.log(this.searchResult);
 
-      if (
-        this.searchResult &&
-        typeof this.searchResult.hits[0] != 'undefined'
-      ) {
-        await Promise.all(
-          this.searchResult.hits.map(async (item) => {
-            item.value = item.name.replace(/[\-\_]+/g, ' ');
-            item.thumb = this.isVideo
-              ? await this.getItemThumbnail(item)
-              : null;
-            item.pUrl = item.url.match(/.*(?=\.)/).toString();
-            item.pName = await this.getParentName(item.pUrl);
-          })
-        );
-        if (this.playerService.playerWidgetLocation === 0) {
-          this.playerService.playerWidgetLocation$.next(1);
-        }
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  }
+			if (
+				this.searchResult &&
+				typeof this.searchResult.hits[0] != 'undefined'
+			) {
+				await Promise.all(
+					this.searchResult.hits.map(async (item) => {
+						item.value = item.name.replace(/[\-\_]+/g, ' ');
+						item.thumb = this.isVideo
+							? await this.getItemThumbnail(item)
+							: null;
+						item.pUrl = item.url.match(/.*(?=\.)/).toString();
+						item.pName = await this.getParentName(item.pUrl);
+					})
+				);
+				if (this.playerService.playerWidgetLocation === 0) {
+					this.playerService.playerWidgetLocation$.next(1);
+				}
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  searchKeyPress(e) {
-    if (e.key === 'Enter' && this.searchQuery) {
-      this.searchMore(this.searchQuery);
-      this.searchOnFocus = false;
-    }
-  }
+	searchKeyPress(e) {
+		if (e.key === 'Enter' && this.searchQuery) {
+			this.searchMore(this.searchQuery);
+			this.searchOnFocus = false;
+		}
+	}
 
-  setFocus(focus) {
-    setTimeout(() => {
-      this.searchOnFocus = focus;
-    }, 150);
-  }
+	setFocus(focus) {
+		setTimeout(() => {
+			this.searchOnFocus = focus;
+		}, 150);
+	}
 
-  selectItem(item) {
-    console.log('item clicked', item);
-    const itemUrl = item.url.replace(/.*\./, '');
-    this.router.navigate(
-      [
-        '/tabs',
-        this.isVideo === true ? 'video' : 'audio',
-        'playlist',
-        item.pUrl,
-      ],
-      {
-        queryParams: { item: itemUrl },
-      }
-    );
-  }
+	selectItem(item) {
+		console.log('item clicked', item);
+		const itemUrl = item.url.replace(/.*\./, '');
+		this.router.navigate(
+			[
+				'/tabs',
+				this.isVideo === true ? 'video' : 'audio',
+				'playlist',
+				item.pUrl,
+			],
+			{
+				queryParams: { item: itemUrl },
+			}
+		);
+	}
 
-  public searchMore(param) {
-    // console.log(this.home, param);
+	public searchMore(param) {
+		// console.log(this.home, param);
 
-    // if (!this.home.includes('favorite') && !this.home.includes('document')) {
-    this.router.navigate(
-      ['/tabs', this.isVideo === true ? 'video' : 'audio', 'search'],
-      {
-        queryParams: { param: param },
-      }
-    );
-    // }
-    this.playerService.playerWidgetLocation$.next(2);
-  }
+		// if (!this.home.includes('favorite') && !this.home.includes('document')) {
+		this.router.navigate(
+			['/tabs', this.isVideo === true ? 'video' : 'audio', 'search'],
+			{
+				queryParams: { param: param },
+			}
+		);
+		// }
+		this.playerService.playerWidgetLocation$.next(2);
+	}
 
-  backNav() {
-    this.location.back();
-  }
+	backNav() {
+		this.location.back();
+	}
 
-  forwardNav() {
-    this.location.forward();
-  }
+	forwardNav() {
+		this.location.forward();
+	}
 
-  private async getParentName(pUrl) {
-    return await this.dataFetchService
-      .fetchSingleTopic(pUrl)
-      .then((item) => item.name.replace(/[\-\_]+/g, ' '));
-  }
+	private async getParentName(pUrl) {
+		return await this.dataFetchService
+			.fetchSingleTopic(pUrl)
+			.then((item) => item.name.replace(/[\-\_]+/g, ' '));
+	}
 
-  private async getItemThumbnail(item: any) {
-    return await this.dataFetchService.getThumbnailUrl(item); // 'https://stream.vgm.tv/VGMV/01_BaiGiang/CacDienGia/MSNHB_DeHiepMotTrongPhucVu/preview/01.jpg';
-  }
+	private async getItemThumbnail(item: any) {
+		return await this.dataFetchService.getThumbnailUrl(item); // 'https://stream.vgm.tv/VGMV/01_BaiGiang/CacDienGia/MSNHB_DeHiepMotTrongPhucVu/preview/01.jpg';
+	}
 
-  async preloadData(item: Item) {
-    if (
-      item.isLeaf === null &&
-      this.dataFetchService.prefetchList.findIndex((elem) => elem === item.id) <
-      0
-    ) {
-      const playUrl = await this.dataFetchService.getPlayUrl(item, true);
-      const dirUrl = path.dirname(playUrl);
-      this.dataFetchService.prefetchList.push(item.id);
-      (async () => {
-        await this.queueService.queue.add(async () => {
-          const fetchItems = [
-            '360p.m3u8',
-            'playlist.m3u8',
-            '360p/content0.vgmx',
-          ];
-          fetchItems.forEach((item) => {
-            const url = `${dirUrl}/${item}`;
-            fetch(url);
-          });
-        });
-      })();
-    }
-  }
+	async preloadData(item: Item) {
+		if (
+			item.isLeaf === null &&
+			this.dataFetchService.prefetchList.findIndex((elem) => elem === item.id) <
+			0
+		) {
+			const playUrl = await this.dataFetchService.getPlayUrl(item, true);
+			const dirUrl = path.dirname(playUrl);
+			this.dataFetchService.prefetchList.push(item.id);
+			(async () => {
+				await this.queueService.queue.add(async () => {
+					const fetchItems = [
+						'360p.m3u8',
+						'playlist.m3u8',
+						'360p/content0.vgmx',
+					];
+					fetchItems.forEach((item) => {
+						const url = `${dirUrl}/${item}`;
+						fetch(url);
+					});
+				});
+			})();
+		}
+	}
 }
--- libs/xplat/ionic/features/src/lib/ui/components/top-menu/top-menu.component.ts
+++ libs/xplat/ionic/features/src/lib/ui/components/top-menu/top-menu.component.ts
@@ -7,108 +7,108 @@ import { IonSlides } from '@ionic/angular';
 import { PlayerService } from 'libs/xplat/core/src/lib/services/player.service';
 
 export interface TopMenuItem {
-  key: string;
-  value: string;
-  href: string;
+	key: string;
+	value: string;
+	href: string;
 }
 
 @Component({
-  selector: 'fy-top-menu',
-  templateUrl: 'top-menu.component.html',
-  styleUrls: ['./top-menu.component.scss'],
+	selector: 'fy-top-menu',
+	templateUrl: 'top-menu.component.html',
+	styleUrls: ['./top-menu.component.scss'],
 })
 
 export class TopMenuComponent extends BaseComponent implements OnInit, OnChanges {
-  @ViewChild(IonSlides) slides: IonSlides
-  @Input() home = '';
-  @Input() activeId: string;
-  private _dataInit = false;
-  public activeHref = '';
+	@ViewChild(IonSlides) slides: IonSlides
+	@Input() home = '';
+	@Input() activeId: string;
+	private _dataInit = false;
+	public activeHref = '';
 
-  public menuList: TopMenuItem[] = [];
-  // topMenuActive = true;
-  constructor(
-    private router: Router,
-    private dataFetchService: DataFetchService,
-    private playerService: PlayerService
-  ) {
-    super();
+	public menuList: TopMenuItem[] = [];
+	// topMenuActive = true;
+	constructor(
+		private router: Router,
+		private dataFetchService: DataFetchService,
+		private playerService: PlayerService
+	) {
+		super();
 
-    this.router.events.subscribe(async (event) => {
-      if (
-        event instanceof NavigationEnd &&
-        event.url.includes(`/tabs/${this.home}/`)
-      ) {
-        if (!this._dataInit) await this.dataInit();
-        this.activeHref = event.url
-          .split('/')
-          .pop()
-          .match(/(?!.*[^?item=]=)(?!\=).*/)
-          .toString();
-        const tabIndex = this.menuList.findIndex(list => this.activeHref.startsWith(list.href));
-        if (this.activeHref && this.menuList && this.slides) {
-          this.slides.getSwiper().then(swiper => {
-            swiper.slideTo(tabIndex)
-          })
-        }
-      }
-    });
+		this.router.events.subscribe(async (event) => {
+			if (
+				event instanceof NavigationEnd &&
+				event.url.includes(`/tabs/${this.home}/`)
+			) {
+				if (!this._dataInit) await this.dataInit();
+				this.activeHref = event.url
+					.split('/')
+					.pop()
+					.match(/(?!.*[^?item=]=)(?!\=).*/)
+					.toString();
+				const tabIndex = this.menuList.findIndex(list => this.activeHref.startsWith(list.href));
+				if (this.activeHref && this.menuList && this.slides) {
+					this.slides.getSwiper().then(swiper => {
+						swiper.slideTo(tabIndex - 1)
+					})
+				}
+			}
+		});
 
 
-    // this.router.events.subscribe(event => {
-    //   if (event instanceof NavigationEnd) {
-    //     if (event.url.includes('/search?param=')) {
-    //       this.topMenuActive = false
-    //     } else {
-    //       this.topMenuActive = true
-    //     }
-    //   }
-    // })
-  }
+		// this.router.events.subscribe(event => {
+		//   if (event instanceof NavigationEnd) {
+		//     if (event.url.includes('/search?param=')) {
+		//       this.topMenuActive = false
+		//     } else {
+		//       this.topMenuActive = true
+		//     }
+		//   }
+		// })
+	}
 
-  async ngOnChanges(changes: SimpleChanges) {
-    if (!this._dataInit) await this.dataInit();
-    if ('activeId' in changes) {
-      const tabIndex = this.menuList.findIndex(list => list.key === this.activeId);
-      this.activeHref = this.menuList[tabIndex].href;
-      if (this.activeId && this.menuList && this.slides) {
-        this.slides.getSwiper().then(swiper => {
-          swiper.slideTo(tabIndex - 1)
-        })
-      }
-    }
-  }
+	async ngOnChanges(changes: SimpleChanges) {
+		if (!this._dataInit) await this.dataInit();
+		if ('activeId' in changes) {
+			const tabIndex = this.menuList.findIndex(list => list.key === this.activeId);
+			this.activeHref = this.menuList[tabIndex].href;
+			if (this.activeId && this.menuList && this.slides) {
+				this.slides.getSwiper().then(swiper => {
+					swiper.slideTo(tabIndex - 1)
+				})
+			}
+		}
+	}
 
-  async ngOnInit() {
-    if (!this._dataInit) await this.dataInit();
-  }
+	async ngOnInit() {
+		if (!this._dataInit) await this.dataInit();
+	}
 
-  async dataInit() {
-    const fetchVideo = this.dataFetchService.fetchRoot('video').then(async list => {
-      return list.map((item) => ({
-        key: item.id,
-        value: item.name.replace(/[0-9]+\-/g, ''),
-        href: item.url,
-      }));
-    });
+	async dataInit() {
+		const fetchVideo = this.dataFetchService.fetchRoot('video').then(async list => {
+			return list.map((item) => ({
+				key: item.id,
+				value: item.name.replace(/[0-9]+\-/g, ''),
+				href: item.url,
+			}));
+		});
 
-    const fetchAudio = this.dataFetchService.fetchRoot('audio').then(list => {
-      return list.map((item) => ({
-        key: item.id,
-        value: item.name.replace(/[0-9]+\-/g, ''),
-        href: item.url,
-      }));
-    });
-    const [vList, aList] = await Promise.all([fetchVideo, fetchAudio]);
-    this.menuList = this.home === 'video' ? vList : this.home === 'audio' ? aList : [];
-    this.activeId = this.menuList && this.menuList.length > 0 ? this.menuList[0].key : '';
-    this._dataInit = true;
-  }
+		const fetchAudio = this.dataFetchService.fetchRoot('audio').then(list => {
+			return list.map((item) => ({
+				key: item.id,
+				value: item.name.replace(/[0-9]+\-/g, ''),
+				href: item.url,
+			}));
+		});
+		const [vList, aList] = await Promise.all([fetchVideo, fetchAudio]);
+		this.menuList = this.home === 'video' ? vList : this.home === 'audio' ? aList : [];
+		this.activeId = this.menuList && this.menuList.length > 0 ? this.menuList[0].key : '';
+		this._dataInit = true;
+	}
 
-  itemClick() {
-    if (this.playerService.playerWidgetLocation === 0) {
-      this.playerService.playerWidgetLocation$.next(1);
-    }
-  }
+	itemClick() {
+		if (this.playerService.playerWidgetLocation === 0) {
+			this.playerService.playerWidgetLocation$.next(1);
+		}
+	}
 
 }
